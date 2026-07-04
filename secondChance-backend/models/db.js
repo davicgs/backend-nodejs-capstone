392697657d09c16eb853136e38c0c8446a37e0b9
davicgs <davicaetanogs@gmail.com>
2026-07-04 17:52:40 -0400

    configured routes and endpoints

--- secondChance-backend/models/db.js
+++ secondChance-backend/models/db.js
@@ -12,17 +12,17 @@ async function connectToDatabase() {
     if (dbInstance){
         return dbInstance
     };
-
+    console.log("My MongoDB URL is:", process.env.MONGO_URL);
     const client = new MongoClient(url);      
 
     // Task 1: Connect to MongoDB
-    // {{insert code}}
+    await client.connect();
 
     // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+    dbInstance = client.db(dbName)
 
     // Task 3: Return database instance
-    // {{insert code}}
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
